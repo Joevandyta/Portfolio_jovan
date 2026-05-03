b1e26f054df55f73015d914eba29569dc4cc0eb1
Joevandyta <joevandyta@gmail.com>
2026-05-03 19:49:53 +0800

    chore: use lowercase URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio_jovan/',
+  base: '/portfolio_jovan/',
   plugins: [
     react(),
     tailwindcss(),
